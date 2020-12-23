0cef5a1d818bce14e39691068d6fa99d72b9bac2
Tarik Čolakhodžić <tarik.colakhodzic@edu.fit.ba>
2020-12-23 12:03:09 +0100

    Added new area - administrator modul

--- TAM.Web/Startup.cs
+++ TAM.Web/Startup.cs
@@ -66,6 +66,9 @@ namespace TAM.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
