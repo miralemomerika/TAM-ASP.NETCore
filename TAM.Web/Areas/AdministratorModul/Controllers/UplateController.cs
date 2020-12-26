8122c189d8594df56b7ad242f83c8c2b8d14d70d
Tarik Čolakhodžić <tarik.colakhodzic@edu.fit.ba>
2020-12-26 23:04:00 +0100

    SvrhaUplate crud implemented

--- TAM.Web/Areas/AdministratorModul/Controllers/UplateController.cs
+++ TAM.Web/Areas/AdministratorModul/Controllers/UplateController.cs
@@ -6,14 +6,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using TAM.Service.Classes;
 using TAM.Service.Interfaces;
-using TAM.ViewModels;
 
 namespace TAM.Web.Areas.AdministratorModul.Controllers
 {
     [Area("AdministratorModul")]
     public class UplateController : Controller
     {
-        ISvrhaUplateService SvrhaUplateService;
+        readonly ISvrhaUplateService SvrhaUplateService;
 
         public UplateController(ISvrhaUplateService svrhaUplateService)
         {
@@ -29,9 +28,54 @@ namespace TAM.Web.Areas.AdministratorModul.Controllers
             }).ToList();
 
             TempData["naslov"] = "Svrha uplate";
-            TempData["urlUredi"] = "/neki/url";
+            TempData["urlUredi"] = "/AdministratorModul/Uplate/SvrhaUplateUredi";
+            TempData["urlDodaj"] = "/AdministratorModul/Uplate/SvrhaUplateDodaj";
+            TempData["urlObrisi"] = "/AdministratorModul/Uplate/SvrhaUplateObrisi";
 
             return View("/Areas/Shared/SelectListItemPrikaz.cshtml", podaci);
         }
+
+        public IActionResult SvrhaUplateUredi(int Id)
+        {
+            var svrha = SvrhaUplateService.GetById(Id);
+
+            TempData["action"] = "SvrhaUplateSpasi";
+            TempData["controller"] = "Uplate";
+
+            return View("/Areas/Shared/SelectListItemForma.cshtml", 
+                new SelectListItem { Value=svrha.Id.ToString(), Text=svrha.Svrha });
+        }
+
+        public IActionResult SvrhaUplateDodaj()
+        {
+            TempData["action"] = "SvrhaUplateSpasi";
+            TempData["controller"] = "Uplate";
+
+            return View("/Areas/Shared/SelectListItemForma.cshtml",
+                new SelectListItem { Value = "0" });
+        }
+
+        public IActionResult SvrhaUplateSpasi(SelectListItem svrha)
+        {
+            if (svrha.Value == "0")
+            {
+                SvrhaUplateService.Add(new Core.SvrhaUplate { Svrha = svrha.Text });
+            }
+            else
+            {
+                var uredi = SvrhaUplateService.GetById(Int32.Parse(svrha.Value));
+                uredi.Svrha = svrha.Text;
+                SvrhaUplateService.Update(uredi);
+            }
+            return RedirectToAction("SvrhaUplatePrikaz");
+        }
+
+        public IActionResult SvrhaUplateObrisi(int Id)
+        {
+            SvrhaUplateService.Delete(SvrhaUplateService.GetById(Id));
+            return RedirectToAction("SvrhaUplatePrikaz");
+        }
+
+
     }
 }
