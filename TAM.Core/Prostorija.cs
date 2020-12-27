b5be0fceeabfc82d8726cfb891850767a381947d
Tarik Čolakhodžić <tarik.colakhodzic@edu.fit.ba>
2020-12-27 22:28:13 +0100

    Prostorije added

---
+++ TAM.Core/Prostorija.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TAM.Core
+{
+    public class Prostorija
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public int BrojMjesta { get; set; }
+    }
+}
---
+++ TAM.Web/Areas/AdministratorModul/Controllers/ProstorijeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TAM.Service.Interfaces;
+
+namespace TAM.Web.Areas.AdministratorModul.Controllers
+{
+    [Area("AdministratorModul")]
+    public class ProstorijeController : Controller
+    {
+        readonly IProstorijaService ProstorijaService;
+        public ProstorijeController(IProstorijaService prostorijaService)
+        {
+            ProstorijaService = prostorijaService;
+        }
+
+        public IActionResult Prikaz()
+        {
+            var podaci = ProstorijaService.GetAll().ToList();
+
+            return View(podaci);
+        }
+    }
+}
