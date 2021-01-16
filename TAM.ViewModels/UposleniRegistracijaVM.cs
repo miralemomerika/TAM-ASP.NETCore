78b4cf4139dbd469bd57f292dbef1b22a2dfcd8b
Tarik Čolakhodžić <tarik.colakhodzic@edu.fit.ba>
2021-01-16 21:58:33 +0100

    Uposleni controller and registration action added

---
+++ TAM.ViewModels/UposleniRegistracijaVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TAM.ViewModels
+{
+    public class UposleniRegistracijaVM
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Broj telefona")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Lozinka")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potvrdi lozinku")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Ime")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Prezime")]
+        public string LastName { get; set; }
+    }
+}
---
+++ TAM.Web/Areas/AdministratorModul/Controllers/UposleniController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TAM.Core;
+using TAM.ViewModels;
+
+namespace TAM.Web.Areas.AdministratorModul.Controllers
+{
+    [Area("AdministratorModul")]
+    public class UposleniController : Controller
+    {
+
+        private readonly SignInManager<KorisnickiRacun> _signInManager;
+        private readonly UserManager<KorisnickiRacun> _userManager;
+        //private readonly ILogger<RegisterModel> _logger;
+        //private readonly IEmailSender _emailSender;
+
+        public UposleniController(
+            UserManager<KorisnickiRacun> userManager,
+            SignInManager<KorisnickiRacun> signInManager
+            //,ILogger<RegisterModel> logger,
+            //IEmailSender emailSender
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            //_logger = logger;
+            //_emailSender = emailSender;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Registracija()
+        {
+            return View(new UposleniRegistracijaVM());
+        }
+        public async Task<IActionResult> Registruj(UposleniRegistracijaVM Input)
+        {
+            //returnUrl = returnUrl ?? Url.Content("~/");
+            //ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            if (ModelState.IsValid)
+            {
+                var user = new KorisnickiRacun
+                {
+                    UserName = Input.Email,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    PhoneNumber = Input.PhoneNumber
+                };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                if (result.Succeeded)
+                {
+                    //_logger.LogInformation("User created a new account with password.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code },
+                        protocol: Request.Scheme);
+
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                       // $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                    }
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View("Index");
+        }
+    }
+}
