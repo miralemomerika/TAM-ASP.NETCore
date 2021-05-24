27e6c767cad5ed7ea348d34a8fc9e0228046fb40
Tarik Čolakhodžić <tarik.colakhodzic@edu.fit.ba>
2021-05-24 23:32:06 +0200

    Kursevi try catch added

--- TAM.API/Controllers/KurseviController.cs
+++ TAM.API/Controllers/KurseviController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TAM.Service.Interfaces;
+using TAM.Web.Helper;
 
 namespace TAM.Web.Controllers
 {
@@ -15,36 +16,57 @@ namespace TAM.Web.Controllers
     {
         private IKursService _service;
         private IOrganizacijaKursaService _organizacijaKursaService;
+        private IExceptionHandlerService _exceptionHandlerService;
 
-        public KurseviController(IKursService service, IOrganizacijaKursaService organizacijaKursaService)
+        public KurseviController(IKursService service, IOrganizacijaKursaService organizacijaKursaService,
+            IExceptionHandlerService exceptionHandlerService)
         {
             _service = service;
             _organizacijaKursaService = organizacijaKursaService;
+            _exceptionHandlerService = exceptionHandlerService;
         }
 
         [HttpGet]
         public IActionResult Kursevi()
         {
-            List<string> includes = new List<string>();
-            includes.Add("KategorijaKursa");
-            var kursevi = _service.GetAll().AsQueryable();
-            kursevi = kursevi.Include(x => x.KategorijaKursa);
-            return Ok(kursevi);
+            try
+            {
+                List<string> includes = new List<string>();
+                includes.Add("KategorijaKursa");
+                var kursevi = _service.GetAll().AsQueryable();
+                kursevi = kursevi.Include(x => x.KategorijaKursa);
+                return Ok(kursevi);
+            }
+            catch (Exception ex)
+            {
+                _exceptionHandlerService.Add(PomocneMetode.GenerisiException(ex));
+                return BadRequest("Kursevi nisu pronadjeni");
+            }
         }
 
         [HttpGet("{Najpopularniji}")]
         public IActionResult Najpopularniji()
         {
-            var q = _organizacijaKursaService.GetAll().AsQueryable();
-            var lista = q.GroupBy(x => x.KursId).Select(x => new NajpoplarnijiIds{
-                Id = x.Key,
-                BrojPojavljivanja = q.Where(y => y.KursId == x.Key).Count()
-            });
-            lista = lista.OrderByDescending(x => x.BrojPojavljivanja);
-            var Ids = lista.Take(3).Select(x => x.Id);
-            var kursevi = _service.GetAll().AsQueryable();
-            kursevi = kursevi.Where(x => Ids.Contains(x.Id)).Include(x => x.KategorijaKursa);
-            return Ok(kursevi);
+            try
+            {
+                var q = _organizacijaKursaService.GetAll().AsQueryable();
+                var lista = q.GroupBy(x => x.KursId).Select(x => new NajpoplarnijiIds
+                {
+                    Id = x.Key,
+                    BrojPojavljivanja = q.Where(y => y.KursId == x.Key).Count()
+                });
+                lista = lista.OrderByDescending(x => x.BrojPojavljivanja);
+                var Ids = lista.Take(3).Select(x => x.Id);
+                var kursevi = _service.GetAll().AsQueryable();
+                kursevi = kursevi.Where(x => Ids.Contains(x.Id)).Include(x => x.KategorijaKursa);
+                return Ok(kursevi);
+            }
+            catch (Exception ex)
+            {
+                _exceptionHandlerService.Add(PomocneMetode.GenerisiException(ex));
+                return BadRequest("Kursevi nisu pronadjeni");
+            }
+            
         }
     }
 
